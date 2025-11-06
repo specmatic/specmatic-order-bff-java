d4d92cdc1c9a320db342569df35a9156b6f0b11b
Sufiyan <StarKhan6368@gmail.com>
2025-11-06 17:32:27 +0530

    Update dependencies

--- src/main/kotlin/com/component/orders/models/Product.kt
+++ src/main/kotlin/com/component/orders/models/Product.kt
@@ -1,8 +1,8 @@
 package com.component.orders.models
 
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
 import java.util.concurrent.atomic.AtomicInteger
-import javax.validation.constraints.NotNull
-import javax.validation.constraints.Positive
 
 data class Product(
     @field:NotNull val name: String = "",
