4c7fa6462da91e7fee9ee49e65826466378e851a
Joel Rosario <cirquitz@gmail.com>
2025-11-12 18:39:34 +0530

    Brought OrderRequest field validations back to previous state

--- src/main/kotlin/com/component/orders/models/OrderRequest.kt
+++ src/main/kotlin/com/component/orders/models/OrderRequest.kt
@@ -4,6 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.validation.constraints.NotNull
 import java.util.UUID
 
-data class OrderRequest(@field:NotNull val productid: Int, @field:NotNull val count: Int) {
+data class OrderRequest(val productid: Int?, val count: Int?) {
     @JsonIgnore val idempotencyKey: String = UUID.randomUUID().toString()
 }
