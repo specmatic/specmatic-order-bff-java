89f584c1752be0a840373cae5b030091c5bb86c0
Sufiyan <StarKhan6368@gmail.com>
2025-11-12 17:59:32 +0530

    Add constraints to request bodies

--- src/main/kotlin/com/component/orders/models/NewProduct.kt
+++ src/main/kotlin/com/component/orders/models/NewProduct.kt
@@ -1,13 +1,15 @@
 package com.component.orders.models
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
 import java.util.UUID
 
 data class NewProduct(
     @field:NotNull val name: String = "",
     @field:NotNull val type: String = "gadget",
-    @field:NotNull val inventory: Int? = 1
+    @field:NotNull @field:Min(1) @field:Max(101) val inventory: Int = 1
 ) {
     @JsonIgnore val idempotencyKey: String = UUID.randomUUID().toString()
 }
