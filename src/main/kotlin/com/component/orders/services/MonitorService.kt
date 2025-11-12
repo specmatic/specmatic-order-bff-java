c8ff2e73e931766441249a859fa2d4ae8e173082
Sufiyan <StarKhan6368@gmail.com>
2025-11-12 14:46:15 +0530

    Update specmatic and mock examples

--- src/main/kotlin/com/component/orders/services/MonitorService.kt
+++ src/main/kotlin/com/component/orders/services/MonitorService.kt
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import java.util.concurrent.atomic.AtomicInteger
 
 @Service
 class MonitorService {
     @Autowired
     lateinit var monitorDatabase: MonitorDatabase
+    private val monitorCycle: AtomicInteger = AtomicInteger(0)
 
     fun retrieveMonitor(monitorId: Int): Monitor<*, *> = monitorDatabase.retrieveMonitor(monitorId)
 
@@ -24,22 +26,29 @@ class MonitorService {
 
     @Scheduled(fixedRate = 2000)
     fun <T, U> scheduledMonitorCheck() {
+        val cycleCount = monitorCycle.getAndIncrement()
         monitorDatabase.transformMonitors { monitorId, monitor ->
-            @Suppress("UNCHECKED_CAST")
-            monitor as Monitor<T, U>
-            if (monitor.response != null) return@transformMonitors monitor
-            if (monitor.request == null) return@transformMonitors monitor
-            println("Invoking monitor with id $monitorId")
-            val response = monitor.callBack.invoke(monitor.request.body)
-            monitor.copy(
-                response = MonitorResponse(
-                    statusCode = response.statusCode.value(),
-                    body = response.body!!,
-                    headers = response.headers.flatMap { (key, values) ->
-                        values.map { value -> HeaderItem(key, value.toString()) }
-                    },
-                ),
-            )
+            try {
+                @Suppress("UNCHECKED_CAST")
+                monitor as Monitor<T, U>
+                if (monitor.response != null) return@transformMonitors monitor
+                if (monitor.request == null) return@transformMonitors monitor
+
+                println("[BFF] Invoking monitor with id $monitorId, In Schedule Cycle $cycleCount")
+                val response = monitor.callBack.invoke(monitor.request.body)
+                monitor.copy(
+                    response = MonitorResponse(
+                        statusCode = response.statusCode.value(),
+                        body = response.body!!,
+                        headers = response.headers.flatMap { (key, values) ->
+                            values.map { value -> HeaderItem(key, value.toString()) }
+                        },
+                    ),
+                )
+            } catch (e: Throwable) {
+                println("[BFF] Monitor $monitorId timed out for id $monitorId: ${e.message}")
+                monitor
+            }
         }
     }
 }
