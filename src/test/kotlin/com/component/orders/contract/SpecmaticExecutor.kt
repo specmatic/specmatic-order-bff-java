1009e259ea47f0bb4f6c0b4dfa4aa0cf5ecd525e
Naresh Jain <nashjain@gmail.com>
2026-02-04 18:08:44 +0530

    refactor: rename command variable and improve error handling for SpecmaticExecutor

--- src/test/kotlin/com/component/orders/contract/SpecmaticExecutor.kt
+++ src/test/kotlin/com/component/orders/contract/SpecmaticExecutor.kt
@@ -5,11 +5,13 @@ import java.io.InputStream
 import java.io.InputStreamReader
 import java.io.PrintStream
 import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.concurrent.TimeUnit
 
-internal class SpecmaticExecutor(arg: String) {
+internal class SpecmaticExecutor(command: String) {
     private val builder: ProcessBuilder
-    private val command: String
+    private val commandName: String
     private var process: Process? = null
     private var stdOut: Thread? = null
     private var stdErr: Thread? = null
@@ -17,14 +19,19 @@ internal class SpecmaticExecutor(arg: String) {
     private val logs = StringBuilder(8192)
 
     init {
-        require(arg.isNotEmpty()) { "At least one argument is required to execute Specmatic" }
-        command = "Specmatic-Enterprise $arg"
+        require(command.isNotBlank()) { "Which Specmatic command do you want to run?" }
+        commandName = "Specmatic-Enterprise $command"
         try {
+            val jarPath = Paths.get(System.getProperty("user.home"), ".specmatic", "specmatic-enterprise.jar")
+            require(Files.isRegularFile(jarPath)) {
+                "Specmatic Enterprise jar not found at ${jarPath.toAbsolutePath()}. " +
+                    "Please visit https://docs.specmatic.io/download/#specmatic-enterprise to fetch the jar."
+            }
             val cmd = buildList {
                 add("java")
                 add("-jar")
-                add("${System.getProperty("user.home")}/.specmatic/specmatic-enterprise.jar")
-                add(arg)
+                add(jarPath.toString())
+                add(command)
             }
             builder = ProcessBuilder(cmd)
         } catch (e: Exception) {
@@ -34,15 +41,15 @@ internal class SpecmaticExecutor(arg: String) {
 
     @Throws(Exception::class)
     fun start() {
-        println("Starting $command")
+        println("Starting $commandName")
         process = builder.start()
-        this.stdOut = startStreamThread(process!!.inputStream, System.out, "$command:STDOUT")
-        this.stdErr = startStreamThread(process!!.errorStream, System.err, "$command:STDERR")
+        this.stdOut = startStreamThread(process!!.inputStream, System.out, "$commandName:STDOUT")
+        this.stdErr = startStreamThread(process!!.errorStream, System.err, "$commandName:STDERR")
     }
 
     @Throws(Exception::class)
     fun stop() {
-        println("Stopping $command")
+        println("Stopping $commandName")
         val current = process ?: return
 
         // wait up to 10s, then forcibly destroy
@@ -65,19 +72,19 @@ internal class SpecmaticExecutor(arg: String) {
 
     @Throws(Exception::class)
     fun verifySuccessfulExecutionWithNoFailures() {
-        println("Verifying $command completed without failures")
-        val current = checkNotNull(process) { "$command process has not been started" }
+        println("Verifying $commandName completed without failures")
+        val current = checkNotNull(process) { "$commandName process has not been started" }
         current.waitFor()
         val exitCode = current.exitValue()
         Assertions.assertThat(exitCode)
             .withFailMessage(
-                String.format("Expected %s to exit without any failures, but it exited with code %%d", command),
+                String.format("Expected %s to exit without any failures, but it exited with code %%d", commandName),
                 exitCode
             )
             .isEqualTo(0)
         val hasSucceeded = logs.toString().contains("Failures: 0")
         Assertions.assertThat(hasSucceeded)
-            .withFailMessage("Expected $command to report 0 failures but some tests have failed")
+            .withFailMessage("Expected $commandName to report 0 failures but some tests have failed")
             .isTrue()
     }
 
